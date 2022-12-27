a7540dbd6ca29878e2ecdc02fb1c23c02099fc0d
jaya-it <114735813+jaya-it@users.noreply.github.com>
2022-12-28 04:23:27 +0800

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -4,14 +4,14 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://youtube.com/c/LeonGanz*`
+let str = `*https://github.com/Bl4ck-lion/kannaxyui*`
 let wibu = `https://api.zacros.my.id/randomimg/waifu` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/leonvx._",
+    mediaUrl: sig,
     mediaType: "VIDEO",
     description: "https://www.instagram.com/reel/Ce_bngYBkEA/?igshid=YmMyMTA2M2Y=", 
-    title: 'Yui-Chan MultiDevice',
+    title: 'ClevvBotz MultiDevice',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sgc
